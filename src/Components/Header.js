9b1cb6810e81d6da574a4e9af821f832c09908c6
yuxuan.jiang <y.x.jiang@hotmail.com>
2019-10-18 21:13:30 +0100

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => (
+  <div>
+    <h1>This is Header</h1>
+  </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: false }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: "8080"
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+      //hash: true
+    })
+  ]
+};
