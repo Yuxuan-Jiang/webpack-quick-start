c233a674be02331f3ded7d74de5db2370d6db5ac
yuxuan.jiang <y.x.jiang@hotmail.com>
2020-01-08 20:22:18 +0000

    add menu data

---
+++ src/menu.js
@@ -0,0 +1,54 @@
+export const menu = {
+  italia: {
+    0: {
+      domainName: "http://emporio-italiano.com",
+      name: "Emporio Italiano",
+      menuUrls: [
+        "http://agenturalacarte.de/",
+        "http://emporio-italiano.de/speisekarten/",
+        "http://emporio-italiano.de/wp-content/uploads/2016/08/Fischkarte.pdf",
+        "http://emporio-italiano.de/wp-content/uploads/2016/08/Vorspeisen.pdf",
+        "http://emporio-italiano.de/wp-content/uploads/2016/09/Dessert-Karte.pdf",
+        "http://emporio-italiano.de/wp-content/uploads/2016/09/Mittagsmenue-online-1.pdf",
+        "http://emporio-italiano.de/wp-content/uploads/2016/09/Wochenkarte-1.pdf",
+        "https://deliveroo.de/de/menu/munchen/neuhausen/emporio-italiano"
+      ]
+    },
+    1: {
+      domainName: "http://piccola-italia.org",
+      name: "Piccola Italia",
+      menuUrls: [
+        "http://piccola-italia.org/upfile/Empfehlungskarte.pdf",
+        "http://piccola-italia.org/upfile/Mittagsmenue.pdf",
+        "http://piccola-italia.org/upfile/Speisekarte.pdf"
+      ]
+    },
+    2: {
+      domainName: "http://restaurant-italy.de",
+      name: "Italy",
+      menuUrls: [
+        "http://restaurant-italy.de/menu",
+        "https://restaurant-italy.de/menu"
+      ]
+    }
+  },
+  steak: {
+    0: {
+      domainName: "http://k-v-r.de",
+      name: "KVR Kapitales vom Rind",
+      menuUrls: [
+        "http://k-v-r.de/pdf/KvR-Abendkarte.pdf",
+        "http://k-v-r.de/pdf/KvR-Tageskarte.pdf",
+        "http://k-v-r.de/pdf/KvR-Wochenkarte_Abend.pdf",
+        "http://k-v-r.de/pdf/KvR-Wochenkarte_Mittag.pdf",
+        "http://k-v-r.de/pdf/Weinkarte.pdf",
+        "https://salsa-verde-streetfood.de"
+      ]
+    },
+    1: {
+      domainName: "http://osteria.de",
+      name: "Osteria Italiana",
+      menuUrls: ["http://osteria.de/speisekarte"]
+    }
+  }
+};
